8a9304412d50c682abeddc82fb55fd822a76e385
Amandeep Sohal <75189240+AmandeepSohal@users.noreply.github.com>
2023-02-08 16:28:43 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,49 @@
+# mad libs
+# this mad lib prompt was taken from "https://www.madlibs.com/wp-content/uploads/2016/04/VacationFun_ML_2009_pg15.pdf"
+print("\nHi, welcome to mad libs. We will make a story out of your inputs:")
+
+print("\nThis MadLib is about Vacation Fun:")
+
+print("\nThis is the prompt:")
+
+print("\nA vacation is when you take a trip to some [ADJECTIVE] place with your [ADJECTIVE] family. Usually you go to some place that is near a/an [NOUN] or up a/an [NOUN]. A good vacation place is one where you can ride [PLURAL NOUN] or play [GAME] or go hunting for [PLURAL NOUN]. I like to spend my time [VERB ENDING IN \"ING\"] or [VERB ENDING IN \"ING\"]. When parents go on a vacation, they spend their time eating three [PLURAL NOUN] a day, and fathers play golf, and mothers sit around [VERB ENDING IN \"ING\"]. Last summer, my little brother fell in a/an [NOUN] and got poison [PLANT] all over his [BODY PART]. My family is going to (the) [A PLACE], and I will practice [VERB ENDING IN \"ING\"]. Parents need more vacations than kids because parents are always very [ADJECTIVE] and because they have to work [NUMBER] hours every day all year making enough [PLURAL NOUN] to pay for the vacation.")
+
+Adjective1 = input("\nEnter an [ADJECTIVE]: ").lower()
+
+Adjective2 = input("\nEnter an [ADJECTIVE]: ").lower()
+
+Noun1 = input("\nEnter a [NOUN]: ").lower()
+
+Noun2 = input("\nEnter a [NOUN]: ").lower()
+
+PluralNoun1 = input("\nEnter a [PLURAL NOUN]: ").lower()
+
+Game = input("\nEnter a [GAME]: ").lower()
+
+PluralNoun2 = input("\nEnter a [PLURAL NOUN]: ").lower()
+
+IngVerb1 = input("\nEnter a [VERB ENDING IN \"ING\"]: ").lower() 
+
+IngVerb2 = input("\nEnter a [VERB ENDING IN \"ING\"]: ").lower() 
+
+PluralNoun3 = input("\nEnter a [PLURAL NOUN]: ").lower()
+
+IngVerb3 = input("\nEnter a [VERB ENDING IN \"ING\"]: ").lower() 
+
+Noun3 = input("\nEnter a [NOUN]: ").lower()
+
+Plant = input("\nEnter a [PLANT]: ").lower()
+
+BodyPart = input("\nEnter a [BODY PART]: ").lower()
+
+Place = input("\nEnter a [PLACE]: ").lower()
+
+IngVerb4 = input("\nEnter a [VERB ENDING IN \"ING\"]: ").lower() 
+
+Adjective3 = input("\nEnter an [ADJECTIVE]: ").lower()
+
+Num = input("\nEnter a [NUMBER]: ").lower()
+
+PluralNoun4 = input("\nEnter a [PLURAL NOUN]: ").lower() 
+
+print(f"\nA vacation is when you take a trip to some {Adjective1} place with your {Adjective2} family. Usually you go to some place that is near a/an {Noun2} or up a/an {PluralNoun1}. A good vacation place is one where you can ride {PluralNoun1} or play {Game} or go hunting for {PluralNoun2}. I like to spend my time {IngVerb1} or {IngVerb2}. When parents go on a vacation, they spend their time eating three {PluralNoun3} a day, and fathers play golf, and mothers sit around {IngVerb3}. Last summer, my little brother fell in a/an {Noun3} and got poison {Plant} all over his {BodyPart}. My family is going to (the) {Place}, and I will practice {IngVerb4}. Parents need more vacations than kids because parents are always very {Adjective3} and because they have to work {Num} hours every day all year making enough {PluralNoun4} to pay for the vacation.")
